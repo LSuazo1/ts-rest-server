0a187365da8de394f3ec869846db53279d5cb442
LSuazo1 <luissuazo80@yahoo.com>
2021-10-06 16:19:48 -0600

    Conexion terminada con la base de datos

--- models/server.ts
+++ models/server.ts
@@ -1,5 +1,7 @@
 import express, { Application } from 'express';
 import useRouter from '../routes/usuarios';
+import cors from 'cors';
+import db from '../db/connection';
 class Server {
 
     private app: Application;
@@ -11,9 +13,29 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8000';
-        
+        //metodos iniciales
+        this.dbConnection();
+        this.middlewares();
         //Definir rutas
         this.routes();
+    }
+
+    async dbConnection(){
+        try {
+            await db.authenticate();
+            console.log('Database online');     
+        } catch (error:any){
+            throw new Error( error );
+        }
+    }
+
+    middlewares(){
+        //CORS 
+        this.app.use(cors());
+        //Lectura del body
+        this.app.use(express.json());
+        //carpeta publica
+        this.app.use(express.static('public'));
     }
 
     routes() {
